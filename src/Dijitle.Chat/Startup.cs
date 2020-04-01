650297174fd9dd7efd5bc9303cc404407f7f7379
Arunas Slepikas <dijitle@gmail.com>
2020-04-01 18:36:37 -0500

    adding swagger, metrics

--- src/Dijitle.Chat/Startup.cs
+++ src/Dijitle.Chat/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Prometheus;
 
 namespace Dijitle.Chat
 {
@@ -26,6 +30,27 @@ namespace Dijitle.Chat
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
+      services.AddMvcCore().AddApiExplorer();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Dijitle Chat API", Version = "v1" });
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        c.IncludeXmlComments(xmlPath);
+
+      });
+
+      services.AddHealthChecks();
+      services.AddCors(c =>
+      {
+        c.AddPolicy("AllowOrigin", b =>
+        {
+          b.WithOrigins(Configuration.GetSection("CORsURLs").Get<string[]>())
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+        });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,17 +60,29 @@ namespace Dijitle.Chat
       {
         app.UseDeveloperExceptionPage();
       }
-
-      app.UseHttpsRedirection();
+      app.UseCors("AllowOrigin");
 
       app.UseRouting();
-
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
+      app.UseStaticFiles();
+      app.UseHttpMetrics();
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
       {
-        endpoints.MapControllers();
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dijitle Chat API V1");
+        c.RoutePrefix = "api";
+        c.EnableDeepLinking();
+        c.EnableFilter();
+        c.InjectJavascript("https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js");
+        c.InjectJavascript("/js/swagger.js");
+        c.InjectStylesheet("/css/swagger.css");
       });
+
+      app.UseEndpoints(e => {
+        e.MapControllers();
+        e.MapMetrics();
+      });
+
+      app.UseHealthChecks("/health");
     }
   }
 }
---
+++ src/Dijitle.Chat/wwwroot/js/swagger.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(setTimeout(function() {
+
+    document.title = 'Dijitle Metra - Swagger UI';
+
+    $("a:first").prop('href', '/');
+
+    $('img:first').prop('alt', 'Dijitle');
+    $('img:first').prop('src', '/Images/dijitle.png');
+    $('img:first').prop('style', 'filter: invert(100%);');
+
+    $("link[rel='icon']").prop('href', '/Images/dijitle.png');
+}, 500));
