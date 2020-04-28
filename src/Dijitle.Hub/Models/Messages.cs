b88131684eead712f5384262ffa158df0bb82cea
Arunas Slepikas <dijitle@gmail.com>
2020-04-28 00:10:29 -0500

    adding messages object

---
+++ src/Dijitle.Hub/Models/Messages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dijitle.Hub.Models
+{
+  public class Messages : LinkedList<Message>
+  {
+    public void AddMessage(string name, string message)
+    {
+      AddLast(new Message(Last?.Value.Id, name, message));
+    }
+
+    public IEnumerable<Message> GetLastMessages(int amount = 25, int page = 1)
+    {
+      return this.SkipLast((page - 1) * amount).TakeLast(amount);
+    }
+
+    public bool IsValid()
+    {
+      foreach(var m in this)
+      {
+        if (!m.IsValid())
+        {
+          return false;
+        }
+      }
+
+      return true;
+    }
+  }
+}
--- tests/Dijitle.Hub.Tests/UnitTest1.cs
+++ tests/Dijitle.Hub.Tests/UnitTest1.cs
@@ -7,28 +7,28 @@ namespace Dijitle.Hub.Tests
 {
   public class Tests
   {
-    private LinkedList<Message> _messages;
+    private Messages _messages;
 
     [SetUp]
     public void Setup()
     {
-      _messages = new LinkedList<Message>();
+      _messages = new Messages();
     }
 
     [Test]
     public void OneMessageIsValid()
     {
-      _messages.AddLast(new Message(null, "Guest 1", "Hello!"));
-      Assert.That(_messages.Select(m => m.IsValid()), Is.All.True);
+      _messages.AddMessage("Guest 1", "Hello!");
+      Assert.That(_messages.IsValid(), Is.True);
     }
 
     [Test]
     public void TwoMessagesAreValid()
     {
-      _messages.AddLast(new Message(null, "Guest 1", "Hello!"));
-      _messages.AddLast(new Message(_messages.Last.Value.Id, "Guest 1", "Hello!"));
+      _messages.AddMessage("Guest 1", "Hello!");
+      _messages.AddMessage("Guest 2", "Hello yourself!");
 
-      Assert.That(_messages.Select(m => m.IsValid()), Is.All.True);
+      Assert.That(_messages.IsValid(), Is.True);
     }
 
     [Test]
@@ -38,7 +38,73 @@ namespace Dijitle.Hub.Tests
 
       _messages.Last.Value.MessageContent = "hello!";
 
-      Assert.That(_messages.Select(m => m.IsValid()), Is.All.False);
+      Assert.That(_messages.IsValid(), Is.False);
+    }
+
+    [Test]
+    public void GetMessageReturnsEmptyIfEmpty()
+    {
+      Assert.That(_messages.GetLastMessages().ToList(), Is.Empty);
+    }
+
+    [Test]
+    public void GetMessageReturnsIfLessThan25()
+    {
+      _messages.AddMessage("Guest1", "Hello!");
+      _messages.AddMessage("Guest2", "Hello!");
+      _messages.AddMessage("Guest3", "Hello!");
+      _messages.AddMessage("Guest4", "Hello!");
+      _messages.AddMessage("Guest5", "Hello!");
+
+      var ms = _messages.GetLastMessages(25, 1).ToList();
+      Assert.That(ms, Has.Count.EqualTo(5));
+    }
+
+    [Test]
+    public void GetMessageReturns3if5Messages()
+    {
+      _messages.AddMessage("Guest1", "Hello!");
+      _messages.AddMessage("Guest2", "Hello!");
+      _messages.AddMessage("Guest3", "Hello!");
+      _messages.AddMessage("Guest4", "Hello!");
+      _messages.AddMessage("Guest5", "Hello!");
+
+      var ms = _messages.GetLastMessages(3, 1).ToList();
+
+      Assert.That(ms, Has.Count.EqualTo(3));
+      Assert.That(ms[0].Name, Is.EqualTo("Guest3"));
+      Assert.That(ms[1].Name, Is.EqualTo("Guest4"));
+      Assert.That(ms[2].Name, Is.EqualTo("Guest5"));
+    }
+
+    [Test]
+    public void GetMessageReturns2if5MessagesTake3onpage2()
+    {
+      _messages.AddMessage("Guest1", "Hello!");
+      _messages.AddMessage("Guest2", "Hello!");
+      _messages.AddMessage("Guest3", "Hello!");
+      _messages.AddMessage("Guest4", "Hello!");
+      _messages.AddMessage("Guest5", "Hello!");
+
+      var ms = _messages.GetLastMessages(3, 2).ToList();
+
+      Assert.That(ms, Has.Count.EqualTo(2));
+      Assert.That(ms[0].Name, Is.EqualTo("Guest1"));
+      Assert.That(ms[1].Name, Is.EqualTo("Guest2"));
+    }
+
+    [Test]
+    public void GetMessageReturns0if5MessagesTake3onpage3()
+    {
+      _messages.AddMessage("Guest1", "Hello!");
+      _messages.AddMessage("Guest2", "Hello!");
+      _messages.AddMessage("Guest3", "Hello!");
+      _messages.AddMessage("Guest4", "Hello!");
+      _messages.AddMessage("Guest5", "Hello!");
+
+      var ms = _messages.GetLastMessages(3, 3).ToList();
+
+      Assert.That(ms, Is.Empty);
     }
   }
 }
